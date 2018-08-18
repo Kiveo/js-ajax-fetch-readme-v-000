f800fa1d99c27bd16ad9cdfb8a8c45107d00a587
Maverick Tesini <mavericktesini@gmail.com>
2018-08-18 06:40:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = '3e14e3d5d25c1bb5b3db2ab7c0067e401064625b';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
